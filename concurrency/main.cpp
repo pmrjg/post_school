050ee3e4c29f4bf36a0e617ce4492afe274a57b3
Pedro Jorge <pmrjge@gmail.com>
2025-01-19 10:33:25 +0000

    Just an example

--- concurrency/main.cpp
+++ concurrency/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include "source/example.h"
 
 int main() {
+    ::run();
     return 0;
 }
---
+++ concurrency/source/example.h
@@ -0,0 +1,30 @@
+//
+// Created by pmrj on 19-01-2025.
+//
+
+#pragma once
+#include <iostream>
+#include <thread>
+
+void foo() {
+  printf("Hello from foo%d\n", std::this_thread::get_id());
+ }
+
+class callable_class {
+public:
+    void operator()() {
+        printf("Hello from class with function call operator%d\n",std::this_thread::get_id());
+    }
+};
+
+ void run() {
+   std::thread thread1(foo);
+    callable_class obj;
+    std::thread thread2(obj);
+     std::thread thread3([=]{printf("Hello from lambda%d\n",std::this_thread::get_id());});
+   thread1.join();
+     thread2.join();
+     thread3.join();
+   printf("Hello from main\n");
+
+ }
